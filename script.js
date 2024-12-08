d37e2177fa2edefc446edfa612b6bc38f5ce67a6
Techchauhan <88832979+Techchauhan@users.noreply.github.com>
2024-12-08 22:53:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// This is the boilerplate code given for you
+// You can modify this code
+// Product data
+const products = [
+  { id: 1, name: "Product 1", price: 10 },
+  { id: 2, name: "Product 2", price: 20 },
+  { id: 3, name: "Product 3", price: 30 },
+  { id: 4, name: "Product 4", price: 40 },
+  { id: 5, name: "Product 5", price: 50 },
+];
+
 // DOM elements
 const productList = document.getElementById("product-list");
 const cartList = document.getElementById("cart-list");
